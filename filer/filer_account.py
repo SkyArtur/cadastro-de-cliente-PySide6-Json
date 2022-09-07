439ca5e03af95543e02d79e24f920eedab1a5777
Artur <sky_artur@hotmail.com>
2022-09-07 15:58:26 -0300

    Criação dos métodos da classe FilerAccount em filer_account.py.

--- filer/filer_account.py
+++ filer/filer_account.py
@@ -1,8 +1,30 @@
 from .filer import Filer
-from managers.objects import Client
 
 
-class FilerAccount(Client, Filer):
+class FilerAccount(Filer):
     def __init__(self):
         super().__init__()
+
+    def save_in_account(self, data):
+        self.save_in("accounts", data)
+
+    def fetch_all_accounts(self):
+        accounts = self.fetch_all("accounts")
+        return accounts[1:]
+
+    def save_in_statement(self, data):
+        self.save_in("statement", data)
+
+    def fetch_all_statement(self):
+        statements = self.fetch_all('statement')
+        return statements
+
+    def save_account_where(self, data):
+        accounts = self.fetch_all("accounts")
+        for i in accounts:
+            if data['id'] == i['id']:
+                i['balance'] = data['balance']
+                i['credits'] = data['credits']
+                i['available'] = data['available']
+        self.save("accounts", accounts)
 
