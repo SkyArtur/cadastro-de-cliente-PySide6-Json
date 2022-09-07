99a87213d0cc8bdd99a6747e879e9aa766e7f3e3
Artur <sky_artur@hotmail.com>
2022-09-07 15:58:55 -0300

    Criação dos métodos da classe FilerClients em filer_clients.py.

--- filer/filer_clients.py
+++ filer/filer_clients.py
@@ -1,7 +1,24 @@
-from .filer import Filer
+from .filer_account import FilerAccount
 
 
-class FilerClients(Filer):
+class FilerClients(FilerAccount):
     def __init__(self):
         super().__init__()
 
+    def define_id_client(self):
+        return self.generator_ids('clients')
+
+    def fetch_all_cpfs(self):
+        cpfs = [i['cpf'] for i in self.fetch_all_clients()]
+        return cpfs
+
+    def fetch_all_names(self):
+        names = [i['name'] for i in self.fetch_all_clients()]
+        return names
+
+    def save_in_clients(self, data):
+        self.save_in("clients", data)
+
+    def fetch_all_clients(self):
+        clients = self.fetch_all("clients")
+        return clients[1:]
