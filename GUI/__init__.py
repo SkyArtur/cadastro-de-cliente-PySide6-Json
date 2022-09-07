7798ee7c3e3402d6b17c8030a230f2b31749c196
Artur <sky_artur@hotmail.com>
2022-09-07 16:08:20 -0300

    Refatoração dos imports do programa

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -1,11 +1,12 @@
 from PySide6.QtWidgets import QApplication
 from sys import exit, argv
 from .gui_login import GUILogin
+from .gui_operations_main import GUIOperationsMain
+from .objects import app
 
 
 def execute_app():
-    app = QApplication(argv)
-    win = GUILogin()
+    win = GUIOperationsMain()
     win.show()
     exit(app.exec())
     
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from GUI import execute_app
-
+from filer import Filer
+from managers import ManagerOperations
+client = {
+        "id": 1,
+        "num": "001-1",
+        "opening": "07/09/2022",
+        "holder": "artur dos santos shon",
+        "balance": 2000.0,
+        "credits": 1000.0,
+        "available": 3000.0
+    }
 
 if __name__ == "__main__":
+    Filer()
     execute_app()
+    # manager = ManagerOperations()
+    # manager.data_client = client
+    # print(manager.get_statement())
+
+
