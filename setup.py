7e7b3c31f9c8e540bf4fded0efcd3cbdd80ebbea
Artur <sky_artur@hotmail.com>
2022-09-08 19:47:43 -0300

    Adição do arquivo de setup.py para criação de executavel.

---
+++ setup.py
@@ -0,0 +1,18 @@
+import sys
+from cx_Freeze import setup, Executable
+
+
+build_exe_options = {"packages": ["os"], "includes": ["PySide6", "pycep_correios"], "excludes": ["tkinter"]}
+
+# base="Win32GUI" should be used only for Windows GUI app
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+    name="Cadastro de Clientes",
+    version="0.1",
+    description="My GUI application!",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("main.py", base=base)],
+)
